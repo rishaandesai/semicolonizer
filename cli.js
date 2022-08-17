bb14605b9c4daa60821b67fbdd3b113c0855497c
Rishaan Desai <77017806+Blackhole1123@users.noreply.github.com>
2022-08-17 16:00:08 -0700

    Revert "New Version"
    This reverts commit 264b506c1f190eb6cf0c0a85d5bd0c4b3adc83e8.

--- cli.js
+++ cli.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
 const semicolonize = require('semicolonizer');
 const file = process.argv[2];
-try {
-    semicolonize(file);
-} catch (error) {
-    console.error(error);
-}
+semicolonize(file);
